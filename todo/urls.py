170c9dded76dc61cc6028fe554ae0e1c38fde537
Calli-T <joy14479@gmail.com>
2022-08-09 14:08:17 +0900

    url정리

--- todo/urls.py
+++ todo/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from .views import TodosAPIView, TodoAPIView, DoneTodosAPIView, DoneTodoAPIView
 
 urlpatterns = [
+    path('', TodosAPIView.as_view()),
     path('todo/', TodosAPIView.as_view()),
     path('todo/<int:pk>', TodoAPIView.as_view()),
     path('done/', DoneTodosAPIView.as_view()),
