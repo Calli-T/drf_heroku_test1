08c0144fd80d9b131f1108ea1b4ceb7a58085924
Calli-T <joy14479@gmail.com>
2022-08-10 19:58:27 +0900

    mariaTest

--- mytodo/settings.py
+++ mytodo/settings.py
@@ -58,13 +58,23 @@ WSGI_APPLICATION = 'mytodo.wsgi.application'
 
 DATABASES = {
     'default': {
+       """
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        """
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sogongDB',
+        'USER': 'sogong',
+        'PASSWORD': 'Sogong2022!?',
+        'HOST' : 'yohanas.synology.me',
+        'PORT': '3306',
     }
 }
 
+"""
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
