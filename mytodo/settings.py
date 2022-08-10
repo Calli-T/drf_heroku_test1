0adf3b8e46aaf25765a126125bb1ce0546d1b53d
Calli-T <joy14479@gmail.com>
2022-08-10 20:01:12 +0900

    mariaTest

--- mytodo/settings.py
+++ mytodo/settings.py
@@ -58,11 +58,7 @@ WSGI_APPLICATION = 'mytodo.wsgi.application'
 
 DATABASES = {
     'default': {
-       """
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        """
-        'ENGINE': 'django.db.backends.mysql',
+       'ENGINE': 'django.db.backends.mysql',
         'NAME': 'sogongDB',
         'USER': 'sogong',
         'PASSWORD': 'Sogong2022!?',
