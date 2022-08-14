deb90b7b7b4c08e84f500a205bb22b5465aa6917
Calli-T <joy14479@gmail.com>
2022-08-14 17:33:33 +0900

    delete

--- board/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import userRegisterAPI, userLoginAPI
-
-urlpatterns = [
-    path('register/', userRegisterAPI.as_view()),
-    path('login/',userLoginAPI.as_view())
-]
